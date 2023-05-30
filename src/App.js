51a1e6b88ba75f83b7e3cb0fbe2204acbcd86479
Craig McLeod <craig.mcleod@2itesting.com>
2023-05-30 09:26:36 +0100

    Push from local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [binArray, setBinArray] = useState(
+    Array(8)
+      .fill(0)
+      .map((x) => x)
+  );
+  const [currentNumber, setCurrentNumber] = useState(0);
+  const helloWorld = "hello world".split("");
+
+  const interval = setInterval(() => {
+    let newCurr = currentNumber + 1;
+    newCurr < helloWorld.length
+      ? setCurrentNumber(newCurr)
+      : setCurrentNumber(0);
+    const bin = helloWorld[currentNumber]
+      .charCodeAt(0)
+      .toString(2)
+      .padStart(8, "0")
+      .split("");
+    setBinArray(bin);
+    clearInterval(interval);
+  }, 500);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="bin-array">
+        {!!binArray.length &&
+          binArray.map((binNo, i) => {
+            return (
+              <span
+                key={binNo + i}
+                className={(!!parseInt(binNo) ? "on" : "off") + " bin"}
+              ></span>
+            );
+          })}
+      </div>
     </div>
   );
 }
